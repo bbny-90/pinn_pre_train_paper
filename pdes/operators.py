db80ced673484c40e9c0f23385b660cdaa48d87d
Bahador Bahmani <bahador@dyn-160-39-162-243.dyn.columbia.edu>
2022-09-16 12:05:33 -0400

    added div operator of tensor field

--- pdes/operators.py
+++ pdes/operators.py
@@ -31,7 +31,7 @@ def get_divergence_vector(
     assert y.dim() == x.dim() == 2
     assert y.shape[0] == x.shape[0]
     num_samples, space_dim = y.shape
-    tmp = torch.zeros_like(y)
+    tmp = torch.zeros(num_samples, 1)
     for i in range(space_dim):
         tmp += grad(y[:,i:i+1], x, torch.ones(num_samples, 1, device=device),
                             create_graph=True, retain_graph=True)[0][:, i:i+1]
@@ -48,3 +48,36 @@ def get_laplace_scalar(
     """
     dydx = get_grad_scalar(y, x, device)
     return get_divergence_vector(dydx, x, device)
+
+
+def get_gradient_vector(
+    y:torch.tensor, x:torch.tensor, device:torch.device = None
+    ) -> torch.tensor:
+    """
+        this function calculates the gradient of a vector_valued
+        function in m-dim space
+        x: independed variable (n, mx)
+        y: dependent variable (n, my)
+    """
+    ndata, ndimY = y.shape
+    ndimX = x.shape[1]
+    out = torch.zeros(ndata, ndimY, ndimX).float().to(device=device)
+    for i in range(ndimY):
+        for j in range(ndimX):
+            out[:, i, j] += get_grad_scalar(y[:, i:i+1], x, device)[:, j]
+    return out
+
+def get_divergence_tensor(
+    y:torch.tensor, x:torch.tensor, device:torch.device = None
+    ) -> torch.tensor:
+    """
+        this function calculates the gradient of a vector_valued
+        function in m-dim space
+        x: independed variable (n, m)
+        y: dependent variable (n, m, m)
+    """
+    out = []
+    for k in range(y.shape[-1]):
+        out.append(get_divergence_vector(y[:, :, k], x, device))
+    out = torch.concat(out, dim=1)
+    return out
--- tests/pdes/grad.py
+++ tests/pdes/grad.py
@@ -54,7 +54,95 @@ def test_grad_scalar_2dim():
     assert np.allclose(dydx_true, dydx_autodiff)
     print(f"{inspect.stack()[0][3]} is passed")
 
+def test_grad_vector_2dim():
+    from pdes.operators import get_gradient_vector
+    from helper.other import get_torch_device
+    import torch
+    import numpy as np
+    
+    device = get_torch_device()
+    ndata, ndim = 10, 2
+    x = torch.rand(ndata, ndim).float().to(device=device)
+    x.requires_grad_(True)
+    y0 = x[:, 1] * x[:, 0]
+    y1 = x[:, 0]**2 + x[:, 1]
+    y = torch.stack([y0, y1], dim=1)
+    grad_y = get_gradient_vector(y, x, device).detach().numpy()
+    grad_y_true = np.zeros((ndata, ndim, ndim))
+    x = x.detach().numpy()
+    grad_y_true[:, 0, 0] = x[:, 1]
+    grad_y_true[:, 0, 1] = x[:, 0]
+    grad_y_true[:, 1, 0] = 2. * x[:, 0]
+    grad_y_true[:, 1, 1] = 1.
+    assert np.allclose(grad_y, grad_y_true)
+    print(f"{inspect.stack()[0][3]} is passed")
+
+def test_divergence_tensor_2dim():
+    from pdes.operators import get_gradient_vector, get_divergence_tensor
+    from helper.other import get_torch_device
+    import torch
+    import numpy as np
+    
+    device = get_torch_device()
+    ndata, ndim = 4, 2
+    x = torch.rand(ndata, ndim).float().to(device=device)
+    x.requires_grad_(True)
+    y0 = x[:, 1]**2 * x[:, 0]
+    y1 = x[:, 0]**3 + x[:, 1]
+    y = torch.stack([y0, y1], dim=1)
+    grad_y = get_gradient_vector(y, x, device)
+    div_grad_y = get_divergence_tensor(grad_y, x, device=device).detach().numpy()
+    x = x.detach().numpy()
+    
+    grad_y_true = np.zeros((ndata, ndim, ndim))
+    grad_y_true[:, 0, 0] = x[:, 1]**2
+    grad_y_true[:, 0, 1] = x[:, 0] * x[:, 1] * 2
+    grad_y_true[:, 1, 0] = 3. * x[:, 0]**2
+    grad_y_true[:, 1, 1] = 1.
+
+    div_grad_y_true = np.zeros((ndata, ndim))
+    div_grad_y_true[:, 1] = x[:, 1] * 2
+
+    assert np.allclose(div_grad_y, div_grad_y_true)
+    print(f"{inspect.stack()[0][3]} is passed")
+
+def test_biharmonic_2dim():
+    from pdes.operators import get_gradient_vector, get_divergence_tensor
+    from helper.other import get_torch_device
+    import torch
+    import numpy as np
+    
+    device = get_torch_device()
+    ndata, ndim = 4, 2
+    x = torch.rand(ndata, ndim).float().to(device=device)
+    x.requires_grad_(True)
+    u0 = x[:, 0]**3 + x[:, 1]**3
+    u1 = x[:, 0] * x[:, 1]**2
+    u = torch.stack([u0, u1], dim=1)
+    grad_u = get_gradient_vector(u, x, device)
+    eps = (grad_u + grad_u.transpose(1, 2)) * 0.5
+    div_eps = get_divergence_tensor(eps, x, device=device).detach().numpy()
+    eps = eps.detach().numpy()
+    x = x.detach().numpy()
+    
+    eps_true = np.zeros((ndata, ndim, ndim))
+    eps_true[:, 0, 0] = 3. * x[:, 0]**2
+    eps_true[:, 1, 1] = x[:, 0] * x[:, 1] * 2
+    eps_true[:, 0, 1] = 0.5 * (x[:, 1]**2 * 3 + x[:, 1]**2)
+    eps_true[:, 1, 0] = eps_true[:, 0, 1]
+    assert np.allclose(eps_true, eps)
+
+    div_eps_true = np.zeros((ndata, ndim))
+    div_eps_true[:, 0] = 6. * x[:, 0] + 0.5 * (x[:, 1]* 6. + x[:, 1]* 2.)
+    div_eps_true[:, 1] = 2. * x[:, 0]
+    assert np.allclose(div_eps_true, div_eps)
+
+    print(f"{inspect.stack()[0][3]} is passed")
+
 if __name__ == "__main__":
     test_first_grad_scalar()
     test_second_grad_scalar()
     test_grad_scalar_2dim()
+    test_grad_vector_2dim()
+    test_divergence_tensor_2dim()
+    test_biharmonic_2dim()
